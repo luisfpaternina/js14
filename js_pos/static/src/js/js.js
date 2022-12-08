3d3a2b37c1aae522e57cbe17115e3c013d587ef5
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-12-08 09:16:31 -0500

    add

--- js_pos/static/src/js/js.js
+++ js_pos/static/src/js/js.js
@@ -1,32 +1,35 @@
-odoo.define('button_near_create.kanban_button', function(require) {
-   "use strict";
-
-   var KanbanController = require('web.KanbanController');
-   var KanbanView = require('web.KanbanView');
-   var viewRegistry = require('web.view_registry');
-   var KanbanButton = KanbanController.include({
-       buttons_template: 'button_near_create.button',
-       events: _.extend({}, KanbanController.prototype.events, {
-           'click .open_wizard_action_kanban': '_OpenWizardKanban',
-       }),
-       _OpenWizardKanban: function () {
-       var self = this;
-        this.do_action({
-           type: 'ir.actions.act_window',
-           res_model: 'sale.order.wizard',
-           name :'Open Wizard',
-           view_mode: 'form',
-           view_type: 'form',
-           views: [[false, 'form']],
-           target: 'new',
-           res_id: false,
-       });
+odoo.define('pos_custom_buttons.RewardButton', function(require) {
+'use strict';
+   const { Gui } = require('point_of_sale.Gui');
+   const PosComponent = require('point_of_sale.PosComponent');
+   const { posbus } = require('point_of_sale.utils');
+   const ProductScreen = require('point_of_sale.ProductScreen');
+   const { useListener } = require('web.custom_hooks');
+   const Registries = require('point_of_sale.Registries');
+   const PaymentScreen = require('point_of_sale.PaymentScreen');
+   class CustomRewardButtons extends PosComponent {
+       constructor() {
+           super(...arguments);
+           useListener('click', this.onClick);
+       }
+       is_available() {
+           const order = this.env.pos.get_order();
+           return order
+       }
+       onClick() {
+                Gui.showPopup("ErrorPopup", {
+                       title: this.env._t('Payment Screen Custom Button Clicked'),
+                       body: this.env._t('Welcome to OWL'),
+                   });
+       }
    }
+   CustomRewardButtons.template = 'CustomRewardButtons';
+   ProductScreen.addControlButton({
+       component: CustomRewardButtons,
+       condition: function() {
+           return this.env.pos;
+       },
    });
-   var SaleOrderKanbanView = KanbanView.extend({
-       config: _.extend({}, KanbanView.prototype.config, {
-           Controller: KanbanButton
-       }),
-   });
-   viewRegistry.add('button_in_kanban', SaleOrderKanbanView);
+   Registries.Component.add(CustomRewardButtons);
+   return CustomRewardButtons;
 });
